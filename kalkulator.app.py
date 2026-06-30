5e7102255ad3ea1ffca04b97d5a29cb717d19b37
iislestari689 <iislestari689@gmail.com>
2026-06-30 12:59:52 +0700

    Initialize kalkulator.app.py with library imports
    Added imports for numpy, pandas, and plotly libraries.

---
+++ kalkulator.app.py
@@ -0,0 +1,9 @@
+# 1. Install Library Plotly (Biasanya sudah bawaan di Google Colab)
+# !pip install plotly pandas numpy
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+print("✅ Library Berhasil Dimuat!")
